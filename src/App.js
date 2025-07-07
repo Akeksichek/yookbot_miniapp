4cbcfb5d850faae0395896babd2ad5ef003e1265
Akeksichek <aleksejpankratov12427@gmail.com>
2025-07-07 20:07:03 +0400

    правки

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useMainButton, useThemeParams } from '@tma.js/sdk-react';
 import Main from './components/main';
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
   const mainButton = useMainButton();
   const themeParams = useThemeParams();
 
